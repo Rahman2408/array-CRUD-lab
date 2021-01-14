868985a50e83304bad6bc3f5ac3c31717f448799
Abdelrahman Elsirafy <rahman2408@gmail.com>
2021-01-13 19:08:25 -0500

    commit tree c12e2a6baafd88b435aa2d0b3ad618be61180924

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,47 @@
+require 'pry'
+
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+ Array.new [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+arrays = []
+  arrays << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
-end
+   arrays_two = []
+   arrays_two.unshift("wow")
+end 
+ 
+
 
 def remove_element_from_end_of_array(array)
-  
+
+    arrays = [ 1, "arrays!"]
+    arrays.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  arrays = [ "wow", 2]
+arrays.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  arrays = ["wow", "array" ,"am"]
+arrays[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    arrays = ["wow", "array" ,"am"]
+    arrays[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+    arrays = ["wow", "am","arrays!" ]
+    arrays[-1]
 end
